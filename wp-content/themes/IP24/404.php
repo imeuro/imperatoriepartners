d756259feaeaf3ea92920e559405d244b9e3184e
meuro <mauro.fioravanzi@mondadori.it>
2024-02-22 00:50:24 +0100

    404

--- wp-content/themes/IP24/404.php
+++ wp-content/themes/IP24/404.php
@@ -14,20 +14,21 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ip24' ); ?></h1>
+				<h1 class="page-title"><?php esc_html_e( 'Pagina non trovata.', 'ip24' ); ?></h1>
 			</header><!-- .page-header -->
 
 			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'ip24' ); ?></p>
+				<p><?php esc_html_e( 'Sembra che la pagina che cerchi non sia disponibile.', 'ip24' ); ?></p>
+				<p>puoi Tornare alla <a href="/">Homepage</a>, oppure cercare altre offerte nelle categorie del sito.</p>
 
-					<?php
+					<?php /*
 					get_search_form();
 
 					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
+					*/ ?>
 
 					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'ip24' ); ?></h2>
+						<h2 class="widget-title"><?php esc_html_e( 'Elenco categorie', 'ip24' ); ?></h2>
 						<ul>
 							<?php
 							wp_list_categories(
@@ -45,10 +46,12 @@ get_header();
 
 					<?php
 					/* translators: %1$s: smiley */
+					/*
 					$ip24_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'ip24' ), convert_smilies( ':)' ) ) . '</p>';
 					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$ip24_archive_content" );
 
 					the_widget( 'WP_Widget_Tag_Cloud' );
+					*/
 					?>
 
 			</div><!-- .page-content -->
